bc119a64319a6d07f54fd2b111c85d02b8dc24da
vagnermengali <vagmengali2015@gmail.com>
2024-02-29 20:49:47 -0300

    add typograph reset

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Header = () => {
     return (
         <header className='bg-white flex items-center justify-center shadow'>
-            <div className="container m-auto h-full w-full">
+            <div className="container m-auto h-full w-full py-4">
                 <Image
                     className="w-36 h-14"
                     src="/serasa-logo-full.svg"
