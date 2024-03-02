101f11135a8861a73a3b1e91576848f339859d03
vagnermengali <vagmengali2015@gmail.com>
2024-03-02 00:32:49 -0300

    feat: add new qa

--- src/components/Ui/Form/index.tsx
+++ src/components/Ui/Form/index.tsx
@@ -28,15 +28,12 @@ const Form = () => {
   return (
     <form onSubmit={handleSubmit} className="rounded-lg bg-light-solid w-[390px] h-[506px] flex flex-col items-center justify-center p-6 shadow-lg">
       <Image
-        id='logo-form'
-        role="logo"
-        aria-label='Logo Serasa'
         className="w-20 h-9 mb-4"
         src="/serasa-logo-full.svg"
-        alt="Logo Serasa"
+        alt="Ir para página inicial"
         width={144}
         height={60}
-        title={'Logo Serasa'}
+        title={'Ir para página inicial'}
         loading='eager'
       />
       <DynamicText ariaLabel="Conte o quanto você está satisfeito com nossos serviços" level="p" title="Conte o quanto você está satisfeito com nossos serviços" className="mb-5 text-center text-dark-medium max-w-64">
@@ -66,13 +63,8 @@ const Form = () => {
         })}
       </div>
       <div className="flex flex-col mb-5">
-        <label id='label-nome'
-          role="label"
-          aria-label='Nome' className='body-bold mb-2'>Nome</label>
+        <label className='body-bold mb-2'>Nome</label>
         <input
-          id='input-nome'
-          role="input"
-          aria-label='Nome'
           type="text"
           placeholder='Nome'
           required
@@ -82,13 +74,8 @@ const Form = () => {
         />
       </div>
       <div className="flex flex-col mb-5">
-        <label id='label-comentario'
-          role="label"
-          aria-label='Comentário (Opcional)' className='body-bold mb-2'>Comentário (Opcional)</label>
+        <label className='body-bold mb-2'>Comentário (Opcional)</label>
         <input
-          id='input-comentario'
-          role="input"
-          aria-label='Comentário (Opcional)'
           type="text"
           placeholder='Comentário (Opcional)'
           className="w-52 p-2 border border-gray-300 ease duration-300 hover:border-magenta outline-magenta/0 outline focus:outline-magenta/50 outline-offset-2 outline-4 rounded-lg"
