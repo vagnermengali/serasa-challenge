e49fcaae27e3d262af36b877915f527a5704e222
vagnermengali <vagmengali2015@gmail.com>
2024-03-04 08:18:07 -0300

    feat: serasa project

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    const headers = [];
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV !== "production") {
+      headers.push({
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "index, follow",
+          },
+        ],
+      });
+    }
+    return headers;
+  },
 };
 
 export default nextConfig;
