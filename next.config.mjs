1c409586b825af676273704ac4439aeda8b2ee93
vagnermengali <vagmengali2015@gmail.com>
2024-03-04 07:57:46 -0300

    feat: serasa project

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,6 @@
-const withTM = require("next-transpile-modules")(["middleware"]);
-const modifyXRobotsTag = require("./pages/api/modifyXRobotsTag").default;
-
-module.exports = withTM({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: "/api/(.*)",
-        headers: [{ key: "X-Robots-Tag", value: "" }],
-      },
-    ];
-  },
-  async middleware() {
-    return [modifyXRobotsTag];
-  },
-});
+};
+
+export default nextConfig;
